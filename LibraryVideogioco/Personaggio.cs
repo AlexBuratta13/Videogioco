d3e6894ef660028e2e04f5b330e8366dab563387
AlexBuratta13 <alex,buratta13@gmail.com>
2021-03-27 19:25:07 +0100

    Add project files.

---
+++ LibraryVideogioco/Personaggio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryVideogioco
+{
+    public class Personaggio
+    {
+        public string nome { get; private set; }
+        public int vita { get; private set; }
+        public int esperienza { get; private set; }
+    }
+}
---
+++ LibraryVideogioco/Razza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryVideogioco
+{
+    public abstract class Razza
+    {
+        public int attacco { get; private set; }
+        public int difesa { get; private set; }
+        private Schieramento schieramento;
+    }
+}
